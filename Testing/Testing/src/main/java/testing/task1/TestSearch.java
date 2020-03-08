ada4780bea206380bb0e8f21c491438b77a4e46a
hoangvutung <hoangvutung97tk@gmail.com>
2020-03-08 11:53:59 +0300

    update

---
+++ Testing/Testing/src/main/java/testing/task1/TestSearch.java
@@ -0,0 +1,26 @@
+package testing.task1;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class TestSearch {
+	private WebDriver webdriver;
+	public TestSearch(WebDriver wd) {
+		webdriver=wd;
+	}
+	 public void searchText(String textToSearch) {
+		WebElement elem = webdriver.findElement(By.name("q"));
+		elem.sendKeys(new String[]{textToSearch});
+		elem.submit();
+	 }
+	 public void verifyFirstResult(String correctText) {
+		 List<WebElement> results = webdriver.findElements(By.xpath("//*[@id=\"rso\"]/div[1]/div/div[1]/div/div/div[1]/a/h3"));
+	     assertEquals(correctText, results.get(0).getText());
+	 }
+
+}
---
+++ Testing/Testing/src/test/java/testing/task1/TestSearchTest.java
@@ -0,0 +1,19 @@
+package testing.task1;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestSearchTest {
+
+	@Test
+	public void test() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\pc\\Downloads\\chromedriver.exe");
+		WebDriver wd = new ChromeDriver();//create an instance for webdriver named wd of type chrome
+		wd.get("https://www.google.com");//open google
+		TestSearch search= new TestSearch(wd);
+		search.searchText("Qt Creator IDE");
+		search.verifyFirstResult("Qt Creator IDE");
+		wd.quit();
+	}
+}
